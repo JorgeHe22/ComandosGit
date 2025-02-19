3cb8d8b55e980a6106ed0a0da39cc6904011c8c7
JorgeHe22 <jorgex.herrera24@gmail.com>
2025-02-19 12:42:51 -0500

    ImpresionesPrueba

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Prueba mensaje 1");
+        System.out.println("Prueba mensaje 2");
+        }
     }
-}
