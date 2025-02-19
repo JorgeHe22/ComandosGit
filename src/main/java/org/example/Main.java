2499248bcfa73f48b901884711647df8466cf2c4
sebastiandrx <sebastianduenas05@gmail.com>
2025-02-19 13:02:31 -0500

    Prueba mensaje clonacion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Prueba mensaje 1");
         System.out.println("Prueba mensaje 2");
+
+        System.out.println("Prueba Clonacion a Jorge Herrera, por Sebastian Dueñas");
         }
     }
